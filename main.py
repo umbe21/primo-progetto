704cfe97f13646e8f06b51a1e94ec6c8647b23de
Umberto <umbef21@gmail.com>
2025-05-12 18:45:32 +0200

    Primo commit: calcolatrice base

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Ciao, Git!")
+print("Benvenuto nella Calcolatrice Base!")
+
+num1 = float(input("Inserisci il primo numero: "))
+operatore = input("Inserisci l'operatore (+, -, *, /): ")
+num2 = float(input("Inserisci il secondo numero: "))
+
+if operatore == "+":
+    risultato = num1 + num2
+elif operatore == "-":
+    risultato = num1 - num2
+elif operatore == "*":
+    risultato = num1 * num2
+elif operatore == "/":
+    if num2 != 0:
+        risultato = num1 / num2
+    else:
+        risultato = "Errore: divisione per zero"
+else:
+    risultato = "Operatore non valido"
+
+print("Risultato:", risultato)
